bd13a95bdc782a6adc34468196b431864b7e69d2
Rod Johnson <johnsonroda@gmail.com>
2018-02-19 10:07:59 +1100

    Initial commit from Atomist

---
+++ src/test/java/com/pivotnine/one/Justin1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pivotnine.one;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Justin1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
